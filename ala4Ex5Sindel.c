03bedf9485272da491476c4e582c234dd2aad0f0
Sindel <76047456+SindelLee@users.noreply.github.com>
2026-04-01 19:16:23 -0300

    Add files via upload

---
+++ ala4Ex5Sindel.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int contador = 5;
+
+    while (contador > 0) {
+        printf("%d\n", contador);
+        contador--;
+    }
+     printf("decolar!\n");
+    return 0;
+}
