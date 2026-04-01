95cebc23dcee918c5041e75525efcc87b5bad8a2
Sindel <76047456+SindelLee@users.noreply.github.com>
2026-04-01 19:10:41 -0300

    Add files via upload

---
+++ ex3 aula4Sindel.c
@@ -0,0 +1,18 @@
+// TrabalhoHorasC.
+#include <stdio.h>
+void main() 
+{
+  int 1,2,3, cliente;
+  printf("Digite a opcao do cliente entre 1, 2 e 3");
+  scanf("%d", &cliente);
+  switch (){
+      case 1:
+      printf("Indicar a area do jardim");
+      break;
+      case 2:
+      printf("indicar area ciom brinquedos);
+      case 3:
+      printf("indicar area vip");
+      break;
+  }
+}
